2f0d6d1a13e51c04ec8a59cb584cd51f35b7f098
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2025-11-24 23:21:46 +0000

    Adição de tela de perfil de usuário

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import HomePage from './pages/HomePage';
+import Map from './components/Map';
+import Profile from './pages/Profile';
 
 function App() {
   return (
