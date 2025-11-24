01730f66b16a1bd995e7e86a06af6ecdd0a71f8a
JoaoMatheus27 <165078857+JoaoMatheus27@users.noreply.github.com>
2025-11-24 22:16:36 +0000

    Ajustes tela de registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -72,17 +72,17 @@ export default function Register() {
   }
 
   return (
-    <div className="min-h-screen bg-white flex  flex-col transform scale-115">
-      <main className="flex-1 flex flex-col items-center justify-start h-full">
-        <div className="w-92  h-50 mb-10  mt-30 flex justify-start items-start ">
-          <img src="/logo.png" alt="Logo Move Recife" className=" object-contain w-80 h-80  " />
+    <div className="h-screen bg-white flex  flex-col transform ">
+      <main className="flex-1 flex flex-col items-center justify-start ">
+        <div className="w-92 h-80 -mt-15 flex justify-start items-start ">
+          <img src="/logo.png" alt="Logo Move Recife" className=" object-contain w-100 h-100  " />
         </div>
         <div className=" w-max h-50 mb-15 ">
 
-          <h1 className="text-xl  font-medium  text-black mb-4">Registre-se</h1>
+          <h1 className="text-2xl  font-medium  text-black mb-4">Registre-se</h1>
           <fieldset className="fieldset w-70">
-            <legend className="fieldset-legend text-black">Nome</legend>
-          <label className="input validator input-neutral bg-white rounded-2xl ">
+            <legend className="fieldset-legend text-[18px] text-black">Nome</legend>
+          <label className="input validator h-12 input-neutral bg-white rounded-2xl ">
             <input
               type="text"
               className=" text-black placeholder-gray-400"
@@ -92,8 +92,8 @@ export default function Register() {
               onChange={(e) => setName(e.target.value)}
             />
           </label>
-          <legend className="fieldset-legend text-black">Email</legend>
-          <label className="input validator input-neutral bg-white rounded-2xl ">
+          <legend className="fieldset-legend text-[18px] text-black">Email</legend>
+          <label className="input validator h-12 input-neutral bg-white rounded-2xl ">
             <input
               type="email"
               className=" text-black placeholder-gray-400"
@@ -106,8 +106,8 @@ export default function Register() {
           <div className="validator-hint hidden">Entre com um email valido</div>
            </fieldset>
           <fieldset className="fieldset w-70">
-          <legend className="fieldset-legend  text-black">Senha</legend>
-          <label className="input validator bg-white input-neutral rounded-2xl ">
+          <legend className="fieldset-legend text-[18px] text-black">Senha</legend>
+          <label className="input validator h-12 bg-white input-neutral rounded-2xl ">
             <input
               type="password"
               required
@@ -121,14 +121,14 @@ export default function Register() {
           </label>
           </fieldset>
         </div>
-          <div className="w-70 flex flex-col gap-2 items-center justify-center ">
+          <div className="w-70 flex flex-col gap-4 items-center justify-center ">
               {error && <div className="text-red-600 mb-2">{error}</div>}
               {success && <div className="text-green-600 mb-2">{success}</div>}
               <button
                 type="button"
                 onClick={handleRegister}
                 disabled={loading}
-                className="btn btn-lg  bg-[#15265A] text-white mt-10"
+                className="btn btn-lg  bg-[#15265A] text-white mt-23"
               >
                 {loading ? "Cadastrando..." : "Criar conta"}
               </button>
@@ -137,13 +137,13 @@ export default function Register() {
                 type="button"
                 onClick={handleGoogleLogin}
                 disabled={loading}
-                className="btn bg-white text-black border-[#e5e5e5]"
+                className="btn bg-white text-black h-12 border-[#b9b9b9]"
               >
-                <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                <svg aria-label="Google logo" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                 Login com Google
               </button>
-              <button className="btn bg-black text-white border-black">
-                <svg aria-label="Apple logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1195 1195"><path fill="white" d="M1006.933 812.8c-32 153.6-115.2 211.2-147.2 249.6-32 25.6-121.6 25.6-153.6 6.4-38.4-25.6-134.4-25.6-166.4 0-44.8 32-115.2 19.2-128 12.8-256-179.2-352-716.8 12.8-774.4 64-12.8 134.4 32 134.4 32 51.2 25.6 70.4 12.8 115.2-6.4 96-44.8 243.2-44.8 313.6 76.8-147.2 96-153.6 294.4 19.2 403.2zM802.133 64c12.8 70.4-64 224-204.8 230.4-12.8-38.4 32-217.6 204.8-230.4z"></path></svg>
+              <button className="btn bg-black text-white border-black h-12 w-47">
+                <svg aria-label="Apple logo" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1195 1195"><path fill="white" d="M1006.933 812.8c-32 153.6-115.2 211.2-147.2 249.6-32 25.6-121.6 25.6-153.6 6.4-38.4-25.6-134.4-25.6-166.4 0-44.8 32-115.2 19.2-128 12.8-256-179.2-352-716.8 12.8-774.4 64-12.8 134.4 32 134.4 32 51.2 25.6 70.4 12.8 115.2-6.4 96-44.8 243.2-44.8 313.6 76.8-147.2 96-153.6 294.4 19.2 403.2zM802.133 64c12.8 70.4-64 224-204.8 230.4-12.8-38.4 32-217.6 204.8-230.4z"></path></svg>
                 Login com Apple
               </button>
           </div>
